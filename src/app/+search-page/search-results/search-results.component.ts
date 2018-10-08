67ac7d7fc411aea48d1a40bb0260582bfc8eb5ed
Christian Scheible <christian.scheible@uni-konstanz.de>
2018-10-08 14:52:30 +0200

    Added link with click handler to add quotes to query

--- src/app/+search-page/search-results/search-results.component.ts
+++ src/app/+search-page/search-results/search-results.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 import { RemoteData } from '../../core/data/remote-data';
 import { DSpaceObject } from '../../core/shared/dspace-object.model';
 import { fadeIn, fadeInOut } from '../../shared/animations/fade';
@@ -6,6 +7,7 @@ import { SearchOptions } from '../search-options.model';
 import { SearchResult } from '../search-result.model';
 import { PaginatedList } from '../../core/data/paginated-list';
 import { ViewMode } from '../../core/shared/view-mode.model';
+import { hasValue } from '../../shared/empty.util';
 
 @Component({
   selector: 'ds-search-results',
@@ -34,5 +36,32 @@ export class SearchResultsComponent {
    * The current view mode for the search results
    */
   @Input() viewMode: ViewMode;
+
+  constructor(private router: Router) { }
+
+  /**
+   * Method to change the current query by surrounding it by quotes if not already present
+   */
+  reloadQuery(event: Event) {
+    console.log('foo');
+    event.preventDefault();
+
+    let newQuery;
+    if (this.searchConfig) {
+      newQuery = this.searchConfig.query;
+    }
+
+    if (hasValue(newQuery) && !(newQuery.startsWith('\"') && newQuery.endsWith('\"'))) {
+      newQuery = `"${newQuery}"`;
+    }
+
+    const navigationExtras: NavigationExtras = {
+      queryParams: {
+        query: newQuery
+      },
+      queryParamsHandling: 'merge'
+    };
+    this.router.navigate(['/search'], navigationExtras);
+  }
 
 }
